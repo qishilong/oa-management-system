ed16faa1a6962a243c54ff44992312bb2dda5d41
Sunny-Sea <663253062@qq.com>
2022-09-12 19:00:27 +0800

    04.手机号码登录UI实现

--- src/pages/users/components/AccountLogin.jsx
+++ src/pages/users/components/AccountLogin.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import IconMap from '../../../components/IconMap';
+import { loginRules } from '../../../utils/rules';
 
 const AccountLogin = ({ Input, FormItem }) => {
   return (
     <div>
-      <FormItem>
+      <FormItem name="accountName" rules={loginRules.userRules} hasFeedback>
         <Input placeholder="请输入用户名" prefix={IconMap.userIcon} />
       </FormItem>
-      <FormItem>
-        <Input placeholder="请输入密码" prefix={IconMap.passwordIcon} />
+      <FormItem name="password" rules={loginRules.passwordRules} hasFeedback>
+        <Input.Password
+          placeholder="请输入密码"
+          prefix={IconMap.passwordIcon}
+        />
       </FormItem>
     </div>
   );
---
+++ src/utils/rules/index.js
@@ -0,0 +1,1 @@
+export { loginRules } from './loginRules.js';
